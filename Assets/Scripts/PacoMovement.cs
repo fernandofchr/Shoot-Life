393b3b9411c3917346effb442798d0f1c3fd36f6
fernandofchr <fernandofchr@gmail.com>
2024-10-24 08:56:53 -0600

    Resolviendo Conflictos

--- Assets/Scripts/PacoMovement.cs
+++ Assets/Scripts/PacoMovement.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> upstream/main
 public class PacoMovement : MonoBehaviour
 {
     public GameObject BulletPrefab;
