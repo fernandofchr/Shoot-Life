70751cc0f60dcbd5102a7521fb59305d491621a1
fernandofchr <fernandofchr@gmail.com>
2024-11-05 10:25:04 -0600

    Opciones y transicion base de nivel

---
+++ Assets/Scripts/MenuOpciones.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MenuOpciones : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    public void PantallaCompleta(bool PantallaCompleta){
+        Screen.fullScreen = PantallaCompleta;
+    }
+
+    public void CambiarVolumen(float volumen){
+        audioMixer.SetFloat("Volumen", volumen);
+    }
+
+    public void CambiarCalidad(int index){
+        QualitySettings.SetQualityLevel(index);
+    }
+}
